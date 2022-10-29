88183b311da646062ebf665b5e439c77bccb325f
Ram Basnet <ram.einstein42@gmail.com>
2022-10-28 20:15:41 -0500

    a simple beginner program

---
+++ calc.cpp
@@ -0,0 +1,48 @@
+# include <iostream>
+
+int main(){
+    char op;
+    double num1;
+    double num2;
+    double result;
+
+    std::cout << " ************** CALCULATOR ************ \n";
+    std::cout << "Enter either + - * /";
+    std::cin>> op;
+
+    std::cout << "Enter first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter second number: ";
+    std::cin >> num2;
+
+    switch(op){
+        case '+':
+            result = num1 + num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '-':
+            result = num1 - num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '*':
+            result = num1 * num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        case '/':
+            result = num1 / num2;
+            std::cout << "result: " << result << '\n';
+            break;
+        default:
+            std::cout << "That wasnt a valid response \n";
+            break;
+
+
+
+    }
+
+
+
+    std::cout << "******************************************";
+    return 0;
+}
